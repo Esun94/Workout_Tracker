b9fec6adadc164251d6f1bf4ca846ffe8e74d321
Eric Sun <ericmsun@gmail.com>
2021-10-13 19:16:34 -0400

    saving intial set up

---
+++ models/workout_starter.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      // For the schema of this attribute, define type and default
+      type: Date,
+      default: () => new Date(),
+    },
+    exercises: [
+      // Schema for each item in exercises array
+      {
+        type: {
+          // TODO: For the schema of this attribute, define type and trim and required
+          type: String,
+          trim: true,
+          required: "Enter a Type of Excersize"
+        },
+        name: {
+          // TODO: For the schema of this attribute, define type and trim and required
+          type: String,
+          trim: true,
+          required: "Enter an Excersize "
+        },
+        duration: {
+          // TODO: For the schema of this attribute, define type as number and its required
+          type: Number,
+          trim: true,
+          required: "Please Enter the Duration of Excersize"
+        },
+        weight: {
+          // TODO: For the schema of this attribute, define type as number
+          type: Number
+        },
+        reps: {
+          // TODO: For the schema of this attribute, define type as number
+        },
+        sets: {
+          // TODO: For the schema of this attribute, define type as number
+        },
+        distance: {
+          // TODO: For the schema of this attribute, define type as number
+        },
+      },
+    ],
+  },
+
+);
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
